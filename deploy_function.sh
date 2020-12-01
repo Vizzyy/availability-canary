f207c5bad5894f70fc882b79c595c2f576d79c50
Vizzyy <barnabas.vizy@gmail.com>
2020-11-30 21:02:15 -0500

    deploy script

--- deploy_function.sh
+++ deploy_function.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-FUNC_NAME=$1
-S3_PATH=$2
+FUNC_NAME="availability-canary"
 
 npm i
 zip -r lambda_function.zip index.js node_modules package.json -q
 aws lambda update-function-code --function-name "$FUNC_NAME" --zip-file fileb://lambda_function.zip
 rm lambda_function.zip
-
-aws s3 cp . "$S3_PATH" --recursive --exclude "*" --include "*.html" --exclude "*/*" --acl public-read
 
 #trigger function
 #aws lambda invoke --function-name "$FUNC_NAME" output.json
