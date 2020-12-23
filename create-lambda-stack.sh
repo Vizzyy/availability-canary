05ec7d1e7475da81172438ed3e3755bf26578c98
Vizzyy <barnabas.vizy@gmail.com>
2020-12-23 17:35:14 -0500

    CustomLogging + Packaged dependencies

--- create-lambda-stack.sh
+++ create-lambda-stack.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FUNC_NAME="availability-canary"
+FUNC_NAME="$1"
 
 aws cloudformation delete-stack --stack-name $FUNC_NAME # delete stack
 
-zip -r lambda_function.zip availability-canary.py -q
+zip -r lambda_function.zip availability-canary.py mysql* google proto* six* -q
 sam package --s3-bucket vizzyy-packaging --output-template-file packaged.yml
 aws cloudformation wait stack-delete-complete --stack-name $FUNC_NAME
 sam deploy --template-file packaged.yml --stack-name $FUNC_NAME --capabilities CAPABILITY_IAM
