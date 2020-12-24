4aa9f1d1442f8f626bde1c43b9053e2390a29547
Vizzyy <barnabas.vizy@gmail.com>
2020-12-24 11:56:11 -0500

    Revert "Recreate DB connection on each call incase of fault between executions"
    This reverts commit 6f490a683fa5420e25f5d1092d3cb7b5619eabb0.

--- availability-canary.py
+++ availability-canary.py
@@ -1,4 +1,5 @@
 import datetime
+
 import boto3
 import os
 import urllib3
@@ -51,15 +52,14 @@ SSL_CONFIG = {
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': '/tmp/db-cert',
 }
+db = mysql.connector.connect(**SSL_CONFIG)
+db._ssl['version'] = ssl.PROTOCOL_TLSv1_2
+cursor = db.cursor()
+print("Connected to Database!")
 
 
 def store_log(start_time, success, failure, id):
     try:
-        db = mysql.connector.connect(**SSL_CONFIG)
-        db._ssl['version'] = ssl.PROTOCOL_TLSv1_2
-        cursor = db.cursor()
-        print("Connected to Database!")
-
         now = datetime.datetime.now()
         elapsed = now - start_time
         elapsed_ms = elapsed.total_seconds() * 1000  # elapsed milliseconds
@@ -70,9 +70,6 @@ def store_log(start_time, success, failure, id):
         cursor.execute(sql)
         db.commit()
         print(f"{sql}")
-        cursor.close()
-        db.close()
-        print("Database connection closed.")
     except Exception as e:
         print(e)
 
